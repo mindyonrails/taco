7492dc2931c761795817c7c38d7a7ad8c5aca1f6
Mindy Chen <mindyonrails@gmail.com>
2017-06-14 20:54:08 +0000

    Uploading custom font

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,16 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Splurty
+  module Splurty
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+   # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   end
 end
